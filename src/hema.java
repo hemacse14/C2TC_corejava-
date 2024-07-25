372a9bec37cd093d02bd2088b8e789108ee2a879
Dineshcodesss <imdineshchandru@gmail.com>
2024-07-25 16:37:33 +0530

    string code

---
+++ src/hema.java
@@ -0,0 +1,28 @@
+package Hema2;
+
+public class hema {
+	int id;
+	String name;
+	String dep;
+  public String getname() {
+		return name;
+	}
+	public void setname() {
+		this.name=name;
+	}
+	public String getdep() {
+		return dep;
+	}
+	public void setdep() {
+		this.dep=dep;
+	}
+	public int getid() {
+		return dep;
+	}
+	public void setid() {
+		this.id=id;
+	}
+	
+	
+	
+}
